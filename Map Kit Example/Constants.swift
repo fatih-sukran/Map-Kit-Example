1027dddd922454d1c66c22078fd8ee8af777a9bc
Fatih Şükran <fth.21.81@gmail.com>
2023-09-29 19:00:51 +0300

    constants

---
+++ Map Kit Example/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Map Kit Example
+//
+//  Created by fatih.sukran on 27.09.2023.
+//
+
+import Foundation
+
+class Constants {
+    static let mapPageSegue = "toMapPage"
+    static let locationEntityName = "Locations"
+}
